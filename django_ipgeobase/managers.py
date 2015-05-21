8cef1c09ce6825f23346d7105096ef1022f9c44b
v2020 <yuriy.perminov@gmail.com>
2015-05-21 11:59:06 +0300

    fix error: illegal IP address string passed to inet_aton

--- django_ipgeobase/managers.py
+++ django_ipgeobase/managers.py
@@ -8,5 +8,8 @@ class IPGeoBaseManager(models.Manager):
     def by_ip(self, ip):
         """Отдает объекты для найденных соответствий по ip.
         Причем, наиболее точное совпадение в начале списка"""
-        number = struct.unpack('!L', socket.inet_aton(ip))[0]
+        try:
+            number = struct.unpack('!L', socket.inet_aton(ip))[0]
+        except socket.error:
+            return super(IPGeoBaseManager, self).get_queryset().none()
         return super(IPGeoBaseManager, self).get_query_set().filter(start_ip__lte=number, end_ip__gte=number).order_by('end_ip', '-start_ip')
